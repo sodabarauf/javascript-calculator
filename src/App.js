747577dd603b3520c18687b192b9cacb9295ec22
sodabarauf <sodabarauf4@gmail.com>
2024-08-29 22:33:21 +0430

    added some new functions

--- src/App.js
+++ src/App.js
@@ -1,68 +1,224 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [display, setDisplay] = useState('0');
+  const [answer, setAnswer] = useState("");
+  const [expression, setExpression] = useState("");
+  const et = expression.trim();
 
-  const handleNumber = (event) => {
-    const number = event.target.textContent;
+  const isOperator = (symbol) => {
+    return /[*/+-]/.test(symbol);
+  };
 
-    if(display === '0') {
-      setDisplay(number);
+  const buttonPress = (symbol) => {
+    if (symbol === "clear") {
+      setAnswer("");
+      setExpression("0");
+    } else if (symbol === "negative") {
+      if (answer === "") return;
+      setAnswer(
+        answer.toString().charAt(0) === "-" ? answer.slice(1) : "-" + answer
+      );
+    } else if (symbol === "percent") {
+      if (answer === "") return;
+      setAnswer((parseFloat(answer) / 100).toString());
+    } else if (isOperator(symbol)) {
+      setExpression(et + " " + symbol + " ");
+    } else if (symbol === "=") {
+      calculate();
+    } else if (symbol === "0") {
+      if (expression.charAt(0) !== "0") {
+        setExpression(expression + symbol);
+      }
+    } else if (symbol === ".") {
+      // split by operators and get last number
+      const lastNumber = expression.split(/[-+/*]/g).pop();
+      if (!lastNumber) return;
+      console.log("lastNumber :>> ", lastNumber);
+      // if last number already has a decimal, don't add another
+      if (lastNumber?.includes(".")) return;
+      setExpression(expression + symbol);
     } else {
-      setDisplay(display + number);
+      if (expression.charAt(0) === "0") {
+        setExpression(expression.slice(1) + symbol);
+      } else {
+        setExpression(expression + symbol);
+      }
     }
   };
 
-  const handleOperator = (event) => {
-    const operator = event.target.textContent;
-    setDisplay(display + ' ' + operator + ' ');
-  };
+  const calculate = () => {
+    // if last char is an operator, do nothing
+    if (isOperator(et.charAt(et.length - 1))) return;
+    // clean the expression so that two operators in a row use the last operator
+    const parts = et.split(" ");
+    const newParts = [];
 
-  const handleEqual = () => {
-    setDisplay(eval(display));
-  };
-
-  const handleDecimal = () => {
-   const array = display.split(' ');
-   const lastElement = array[array.length - 1];
-
-   if(lastElement.includes('.')) {
-    setDisplay(display + '.');
-   }
-  };
-
-  const handleClear = () => {
-    setDisplay('0');
+    // go through parts backwards
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (["*", "/", "+"].includes(parts[i]) && isOperator(parts[i - 1])) {
+        newParts.unshift(parts[i]);
+        let j = 0;
+        let k = i - 1;
+        while (isOperator(parts[k])) {
+          k--;
+          j++;
+        }
+        i -= j;
+      } else {
+        newParts.unshift(parts[i]);
+      }
+    }
+    const newExpression = newParts.join(" ");
+    if (isOperator(newExpression.charAt(0))) {
+      setAnswer(eval(answer + newExpression).toString());
+    } else {
+      setAnswer(eval(newExpression).toString());
+    }
+    setExpression("");
   };
 
   return (
-    <div className="App">
-      <div className="calculator">
-        <div id="display" className='row'>
-          {display}
+    <>
+      <div className="container">
+        <h1>Calculator Application</h1>
+        <div id="calculator">
+          <div id="display" style={{ textAlign: "right" }}>
+            <div id="answer">{answer}</div>
+            <div id="expression">{expression}</div>
+          </div>
+          <button
+            id="clear"
+            onClick={() => buttonPress("clear")}
+            className="light-gray"
+          >
+            C
+          </button>
+          <button
+            id="negative"
+            onClick={() => buttonPress("negative")}
+            className="light-gray"
+          >
+            +/-
+          </button>
+          <button
+            id="percentage"
+            onClick={() => buttonPress("percentage")}
+            className="light-gray"
+          >
+            %
+          </button>
+          <button
+            id="divide"
+            onClick={() => buttonPress("/")}
+            className="yellow"
+          >
+            /
+          </button>
+          <button
+            id="seven"
+            onClick={() => buttonPress("7")}
+            className="dark-gray"
+          >
+            7
+          </button>
+          <button
+            id="eight"
+            onClick={() => buttonPress("8")}
+            className="dark-gray"
+          >
+            8
+          </button>
+          <button
+            id="nine"
+            onClick={() => buttonPress("9")}
+            className="dark-gray"
+          >
+            9
+          </button>
+          <button
+            id="multiply"
+            onClick={() => buttonPress("*")}
+            className="yellow"
+          >
+            *
+          </button>
+          <button
+            id="four"
+            onClick={() => buttonPress("4")}
+            className="dark-gray"
+          >
+            4
+          </button>
+          <button
+            id="five"
+            onClick={() => buttonPress("5")}
+            className="dark-gray"
+          >
+            5
+          </button>
+          <button
+            id="six"
+            onClick={() => buttonPress("6")}
+            className="dark-gray"
+          >
+            6
+          </button>
+          <button
+            id="subtract"
+            onClick={() => buttonPress("-")}
+            className="yellow"
+          >
+            -
+          </button>
+          <button
+            id="one"
+            onClick={() => buttonPress("1")}
+            className="dark-gray"
+          >
+            1
+          </button>
+          <button
+            id="two"
+            onClick={() => buttonPress("2")}
+            className="dark-gray"
+          >
+            2
+          </button>
+          <button
+            id="three"
+            onClick={() => buttonPress("3")}
+            className="dark-gray"
+          >
+            3
+          </button>
+          <button id="add" onClick={() => buttonPress("+")} className="yellow">
+            +
+          </button>
+          <button
+            id="zero"
+            onClick={() => buttonPress("0")}
+            className="dark-gray"
+          >
+            0
+          </button>
+          <button
+            id="decimal"
+            onClick={() => buttonPress(".")}
+            className="dark-gray"
+          >
+            .
+          </button>
+          <button
+            id="equals"
+            onClick={() => buttonPress("=")}
+            className="yellow"
+          >
+            =
+          </button>
         </div>
-        <div id="clear" className='row' onClick={handleClear}>
-          AC
-        </div>
-        <div id="seven" onClick={handleNumber}>7</div>
-        <div id="eight" onClick={handleNumber}>8</div>
-        <div id="nine" onClick={handleNumber}>9</div>
-        <div id="multipl" onClick={handleOperator}>*</div>
-        <div id="four" onClick={handleNumber}>4</div>
-      <div id="five" onClick={handleNumber}>5</div>
-      <div id="six" onClick={handleNumber}>6</div>
-      <div id="divide" onClick={handleOperator}>/</div>
-      <div id="one" onClick={handleNumber}>1</div>
-      <div id="two" onClick={handleNumber}>2</div>
-      <div id="three" onClick={handleNumber}>3</div>
-      <div id="add" onClick={handleOperator}>+</div>
-      <div id="zero" onClick={handleNumber}>0</div>
-      <div id="decimal" onClick={handleDecimal}>.</div>
-      <div id="equal" onClick={handleEqual}>=</div>
-      <div id="subtract" onClick={handleOperator}>-</div>
-     </div>
-    </div>
+      </div>
+    </>
   );
 }
 
