20fa0b6af50d05eeb47e2e51fcb6c02e3cd9e893
sodabarauf <sodabarauf4@gmail.com>
2024-08-29 09:19:20 +0430

    added some tags to app.js main component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+     <div id="calculator">
+      <div id="display" className='row'></div>
+      <div id="clear" className='row'>
+        AC
+      </div>
+      <div id="seven">7</div>
+      <div id="eight">8</div>
+      <div id="multipl">*</div>
+      <div id="four">4</div>
+      <div id="five">5</div>
+      <div id="six">6</div>
+      <div id="divide">/</div>
+      <div id="one">1</div>
+      <div id="two">2</div>
+      <div id="three">3</div>
+      <div id="add">+</div>
+      <div id="zero">0</div>
+      <div id="decimal">.</div>
+      <div id="equal">=</div>
+      <div id="subtract">-</div>
+     </div>
     </div>
   );
 }
